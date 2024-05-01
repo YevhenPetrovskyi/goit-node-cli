67b5978b3984306c25c56bf059d4b9c419a45cc7
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-05-01 19:59:20 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -22,23 +22,29 @@ export async function getContactById(contactId) {
 export async function removeContact(contactId) {
   const contacts = await listContacts();
   const idx = contacts.findIndex(contact => contact.id === contactId);
+
   if (idx === -1) {
     return null;
   }
+
   const [result] = contacts.splice(idx, 1);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
   return result;
 }
 
 export async function addContact(name, email, phone) {
   const contacts = await listContacts();
+
   const newContact = {
     id: nanoid(),
     name,
     email,
     phone,
   };
+
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
   return newContact;
 }
