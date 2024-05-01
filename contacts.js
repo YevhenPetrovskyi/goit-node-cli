1a7f56ad04d9a43716041ddc9c4b0b1b1d38870d
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-05-01 20:21:36 +0300

    remove nanoid

--- contacts.js
+++ contacts.js
@@ -1,7 +1,7 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { nanoid } from 'nanoid';
+import crypto from 'crypto';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -37,7 +37,7 @@ export async function addContact(name, email, phone) {
   const contacts = await listContacts();
 
   const newContact = {
-    id: nanoid(),
+    id: crypto.randomUUID(),
     name,
     email,
     phone,
